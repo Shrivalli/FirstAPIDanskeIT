99a05c60c7e31a56fd66f7f06a97f01526c79a00
Shrivalli <shrivalli04@gmail.com>
2023-08-30 16:46:00 +0530

    Add project files.

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,68 @@
+﻿using FirstAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration.EnvironmentVariables;
+
+namespace FirstAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController] //Data Annotation - compiler to understand
+    public class ProductController : ControllerBase
+    {
+        public static List<Product> Products = new List<Product>();
+        public Product p = new Product();
+
+        [HttpGet]
+        public async Task<ActionResult> GetProducts()
+        {
+            Products = await p.GetAllProducts();
+            return Ok(Products);
+        }
+
+        [HttpGet]
+        [Route("GetProdById")]
+        public async Task<ActionResult> GetProductByID(int pid)
+        {
+            if(pid.Equals(0))
+            {
+                return BadRequest("Id not supplied");
+            }
+            var p=Product.GetProductById(pid);
+            if (p == null)
+            {
+                return NotFound("Product not Found");
+            }
+            return Ok(p);
+        }
+
+        [HttpPost]
+        public ActionResult AddNewProduct(Product p)
+        {
+            if (p != null)
+            {
+                Product.AddProduct(p);
+                return Ok();
+            }
+            else
+                return BadRequest("Product not supplied");
+        }
+
+        [HttpDelete]
+        public ActionResult RemoveProduct(int id)
+        {
+            Product.DeleteProduct(id);
+            return Ok();
+        }
+
+        [HttpPut]
+        public ActionResult UpdateProduct(int id,Product p) 
+        {
+            if(id==null)
+            {
+                return BadRequest("Id not provided");
+            }
+            Product.EditProduct(id, p);
+            return Ok();
+        }
+    }
+}
---
+++ Models/Product.cs
@@ -0,0 +1,61 @@
+﻿namespace FirstAPI.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Price { get; set; }
+
+        public static List<Product> Products { get; set; } = new List<Product>() 
+        { new Product(1, "Pen", 34), new Product(2, "Pencil", 78) };
+
+        public Product() { } //default constructor
+
+        public Product(int id, string name, int price) //Parameterized constructor
+        {
+            Id = id;
+            Name = name;
+            Price = price;
+        }
+
+        public async Task<List<Product>> GetAllProducts()
+        {
+            //Products.Add(new Product(1, "Pen", 34));
+            //Products.Add(new Product(2, "Erasor", 48));
+            //Products.Add(new Product(3, "Laptop", 78));
+            //Products.Add(new Product(4, "Pencil", 90));
+            return Products;
+        }
+
+        public static Product GetProductById(int id)
+        {
+           // Products.Clear();
+           // Products =GetAllProducts();
+
+            Product p = Products.Where(x => x.Id == id).FirstOrDefault();
+            return p;
+
+        }
+
+        public static void AddProduct(Product p)
+        {
+            Products.Add(p);
+        }
+
+        public static void DeleteProduct(int id)
+        {
+            var p=GetProductById(id);
+            Products.Remove(p);
+
+        }
+
+        public static void EditProduct(int id, Product p)
+        {
+            var oldprd = GetProductById(id);
+            Products.Remove(oldprd);
+            Products.Add(p);
+        }
+
+            
+    }
+}
